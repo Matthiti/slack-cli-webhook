785901c61c7d50a9dd0c6cf60ac9b69a058f9d9c
Matthiti <mj.roelink@outlook.com>
2019-02-10 16:58:52 +0100

    Improvements

--- slack-cli-webhook/__main__.py
+++ slack-cli-webhook/__main__.py
@@ -1,8 +1,21 @@
 # __main__.py
+from slack_cli import SlackCLI
+from argparse import ArgumentParser
+import sys
 
 
 def main():
-    print("Hello World!")
+    parser = ArgumentParser()
+    parser.add_argument("-m", "--message", dest="message", help="message to send", required=True)
+    parser.add_argument("-w", "--webhook", dest="webhook", help="webhook to send to", required=True)
+    args = parser.parse_args()
+    print(args.message)
+    slack_cli = SlackCLI(args.webhook)
+    if slack_cli.send_message(args.message):
+        print("Message successfully sent.")
+    else:
+        print("Sending message failed, please try again later.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
